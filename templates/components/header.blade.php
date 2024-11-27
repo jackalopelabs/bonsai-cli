ae90f3b9a918a52ebabafeea0d2ac92537bf5baa
Mason Lawlor <mason.lawlor@me.com>
2024-11-27 00:12:07 -0700

    missing scrollHandler Alpine.js component and the missing icon components
    The changes:
    Added Alpine.js scrollHandler component inline
    Added fallback for icon component
    Using heroicons instead of custom icons
    Added error handling for icon component

--- templates/components/header.blade.php
+++ templates/components/header.blade.php
@@ -2,17 +2,36 @@
     'containerClasses' => 'max-w-5xl mx-auto',
     'containerInnerClasses' => 'px-6',
     'siteName' => '',
-    'iconComponent' => 'icon-jackalope',
+    'iconComponent' => 'heroicon-o-cube',
     'navLinks' => [],
     'primaryLink' => '#pricing',
 ])
+
+<script>
+document.addEventListener('alpine:init', () => {
+    Alpine.data('scrollHandler', () => ({
+        scrollTo(anchor) {
+            const element = document.querySelector(anchor);
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    }));
+});
+</script>
 
 <header class="{{ $containerClasses }} bg-opacity-60 backdrop-blur-md shadow-lg border border-transparent rounded-full mx-auto p-1 my-4" x-data="scrollHandler">
     <div class="{{ $containerInnerClasses }} flex justify-between items-center w-full">
         <div class="flex">
             <a class="py-3 font-bold text-lg block" href="{{ home_url('/') }}">
                 <div class="flex items-center">
-                    <x-dynamic-component :component="$iconComponent" class="h-8 w-8 mr-2 p-1" />
+                    @if(str_starts_with($iconComponent, 'heroicon'))
+                        <x-dynamic-component :component="$iconComponent" class="h-8 w-8 mr-2 p-1" />
+                    @else
+                        <div class="h-8 w-8 mr-2 p-1">
+                            <x-heroicon-o-cube class="h-full w-full" />
+                        </div>
+                    @endif
                     {!! $siteName !!}
                 </div>
             </a>
