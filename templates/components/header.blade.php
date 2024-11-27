5ae3608d0bcaf002d5ce08a811943d08f14151e0
Mason Lawlor <mason.lawlor@me.com>
2024-11-27 00:07:51 -0700

    Added header component (was blank)

--- templates/components/header.blade.php
+++ templates/components/header.blade.php
@@ -1,1 +1,36 @@
- 
+@props([
+    'containerClasses' => 'max-w-5xl mx-auto',
+    'containerInnerClasses' => 'px-6',
+    'siteName' => '',
+    'iconComponent' => 'icon-jackalope',
+    'navLinks' => [],
+    'primaryLink' => '#pricing',
+])
+
+<header class="{{ $containerClasses }} bg-opacity-60 backdrop-blur-md shadow-lg border border-transparent rounded-full mx-auto p-1 my-4" x-data="scrollHandler">
+    <div class="{{ $containerInnerClasses }} flex justify-between items-center w-full">
+        <div class="flex">
+            <a class="py-3 font-bold text-lg block" href="{{ home_url('/') }}">
+                <div class="flex items-center">
+                    <x-dynamic-component :component="$iconComponent" class="h-8 w-8 mr-2 p-1" />
+                    {!! $siteName !!}
+                </div>
+            </a>
+        
+            <ul class="flex space-x-4 items-center ml-6 hidden sm:flex">
+                @foreach ($navLinks as $link)
+                    @if (!empty($link['url']) && !empty($link['label']))
+                        <a href="{{ $link['url'] }}" x-on:click.prevent="scrollTo('{{ $link['url'] }}')">
+                            <li>{{ $link['label'] }}</li>
+                        </a>
+                    @endif
+                @endforeach
+            </ul>
+        </div>
+        <div class="flex space-x-4 items-center">
+            <a href="{{ $primaryLink }}" class="btn bg-white py-2 px-4 border border-transparent rounded-full bg-opacity-60 backdrop-blur-md shadow-lg" x-on:click.prevent="scrollTo('{{ $primaryLink }}')">
+                <span class="hidden sm:inline">See</span> Plans <x-heroicon-s-chevron-down class="w-4 h-4 ml-2 inline-block"/>
+            </a>              
+        </div>
+    </div>
+</header> 
