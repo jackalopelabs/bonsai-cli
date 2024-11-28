de88e4d006f217af4f1f564c0a7cb00a8305cdf6
Mason Lawlor <mason.lawlor@me.com>
2024-11-28 16:53:06 -0700

    Fix string interpolation in BonsaiTree model
    - Replace deprecated ${expr} syntax with {$expr} for variable interpolation
    - Fix undefined constant error in string templates
    - Maintain consistent string formatting across all tree styles

--- src/Models/BonsaiTree.php
+++ src/Models/BonsaiTree.php
@@ -28,39 +28,39 @@ class BonsaiTree
         switch ($this->style) {
             case 'formal':
                 $tree = [
-                    "          ${this->leaves[2]}          ",
-                    "         ${this->leaves[0]}${this->leaves[2]}${this->leaves[0]}         ",
-                    "     ${this->leaves[1]}  ${this->leaves[2]}${this->leaves[0]}${this->leaves[2]}  ${this->leaves[1]}     ",
-                    "       ${this->leaves[0]}/${this->trunk[1]}\\${this->leaves[0]}       ",
-                    "        ${this->trunk[1]} ${this->trunk[1]}        ",
-                    "      ${this->branches[1]}${this->trunk[1]}${this->trunk[1]}${this->trunk[1]}${this->branches[3]}      ",
-                    "     ${this->branches[0]}${this->trunk[1]}${this->trunk[1]} ${this->trunk[1]}${this->trunk[1]}${this->branches[2]}     ",
+                    "          {$this->leaves[2]}          ",
+                    "         {$this->leaves[0]}{$this->leaves[2]}{$this->leaves[0]}         ",
+                    "     {$this->leaves[1]}  {$this->leaves[2]}{$this->leaves[0]}{$this->leaves[2]}  {$this->leaves[1]}     ",
+                    "       {$this->leaves[0]}/{$this->trunk[1]}\\{$this->leaves[0]}       ",
+                    "        {$this->trunk[1]} {$this->trunk[1]}        ",
+                    "      {$this->branches[1]}{$this->trunk[1]}{$this->trunk[1]}{$this->trunk[1]}{$this->branches[3]}      ",
+                    "     {$this->branches[0]}{$this->trunk[1]}{$this->trunk[1]} {$this->trunk[1]}{$this->trunk[1]}{$this->branches[2]}     ",
                     "    =============    "
                 ];
                 break;
                 
             case 'informal':
                 $tree = [
-                    "           ${this->leaves[2]}        ",
-                    "     ${this->leaves[0]}  ${this->leaves[2]}${this->leaves[0]}${this->leaves[2]}         ",
-                    "   ${this->leaves[1]}  ${this->leaves[2]}${this->leaves[0]}${this->leaves[2]}  ${this->leaves[1]}     ",
-                    "      ${this->leaves[0]}\\${this->trunk[1]}/${this->leaves[0]}       ",
-                    "       ${this->trunk[1]} ${this->trunk[2]}        ",
-                    "     ${this->branches[1]}${this->trunk[1]}${this->trunk[1]}${this->branches[2]}${this->branches[3]}      ",
-                    "    ${this->branches[0]}${this->trunk[1]}${this->trunk[1]}${this->trunk[1]}${this->branches[2]}     ",
+                    "           {$this->leaves[2]}        ",
+                    "     {$this->leaves[0]}  {$this->leaves[2]}{$this->leaves[0]}{$this->leaves[2]}         ",
+                    "   {$this->leaves[1]}  {$this->leaves[2]}{$this->leaves[0]}{$this->leaves[2]}  {$this->leaves[1]}     ",
+                    "      {$this->leaves[0]}\\{$this->trunk[1]}/{$this->leaves[0]}       ",
+                    "       {$this->trunk[1]} {$this->trunk[2]}        ",
+                    "     {$this->branches[1]}{$this->trunk[1]}{$this->trunk[1]}{$this->branches[2]}{$this->branches[3]}      ",
+                    "    {$this->branches[0]}{$this->trunk[1]}{$this->trunk[1]}{$this->trunk[1]}{$this->branches[2]}     ",
                     "    =============    "
                 ];
                 break;
                 
             case 'slanting':
                 $tree = [
-                    "              ${this->leaves[2]}     ",
-                    "        ${this->leaves[0]}  ${this->leaves[2]}${this->leaves[0]}${this->leaves[2]}      ",
-                    "      ${this->leaves[1]}  ${this->leaves[2]}${this->leaves[0]}${this->leaves[2]}  ${this->leaves[1]}  ",
-                    "        ${this->leaves[0]}\\${this->trunk[2]}/${this->leaves[0]}    ",
-                    "         ${this->trunk[2]} ${this->trunk[2]}     ",
-                    "       ${this->branches[1]}${this->trunk[2]}${this->trunk[2]}${this->branches[2]}    ",
-                    "      ${this->branches[0]}${this->trunk[2]}${this->trunk[2]}${this->branches[2]}   ",
+                    "              {$this->leaves[2]}     ",
+                    "        {$this->leaves[0]}  {$this->leaves[2]}{$this->leaves[0]}{$this->leaves[2]}      ",
+                    "      {$this->leaves[1]}  {$this->leaves[2]}{$this->leaves[0]}{$this->leaves[2]}  {$this->leaves[1]}  ",
+                    "        {$this->leaves[0]}\\{$this->trunk[2]}/{$this->leaves[0]}    ",
+                    "         {$this->trunk[2]} {$this->trunk[2]}     ",
+                    "       {$this->branches[1]}{$this->trunk[2]}{$this->trunk[2]}{$this->branches[2]}    ",
+                    "      {$this->branches[0]}{$this->trunk[2]}{$this->trunk[2]}{$this->branches[2]}   ",
                     "    =============    "
                 ];
                 break;
