794c7164de2f1b7983e67cecc6cc8bc2efa2020b
Mason Lawlor <mason.lawlor@me.com>
2025-02-19 18:02:03 -0700

    Ran Scion Cypress (on scion branch for bonsai.so, used wrong branch last time)

--- templates/layouts/cypress.blade.php
+++ templates/layouts/cypress.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html @php(language_attributes()) class="dark relative h-screen" x-data="{ darkMode: localStorage.getItem("darkMode") === null ? true : localStorage.getItem("darkMode") === "true" }" x-init="$watch("darkMode", val => localStorage.setItem("darkMode", val))" :class="{ "dark": darkMode }">
+<html @php(language_attributes()) class="dark relative h-screen" x-data="{ darkMode: localStorage.getItem('darkMode') === null ? true : localStorage.getItem('darkMode') === 'true' }" x-init="$watch('darkMode', val => localStorage.setItem('darkMode', val))" :class="{ 'dark': darkMode }">
     <!-- Hero Background Images -->
     <div class="absolute inset-0 z-0">
         <img src="{{ asset('images/bonsai_hero_03.png') }}" 
@@ -20,23 +20,19 @@
         @include('bonsai.components.analytics')
         @include('utils.styles')
     </head>
-
     <body @php(body_class('transition-colors duration-200 p-0 m-0 bg-transparent'))>
         @php(wp_body_open())
         <div id="app" class="relative z-10">
             <a class="sr-only focus:not-sr-only" href="#main">
                 {{ __('Skip to content', 'radicle') }}
             </a>
-
-            @includeIf('bonsai.sections.site_header')
-
+            @include('bonsai.cypress.sections.site_header')
             <main id="main" class="max-w-5xl mx-auto">
-                <div class="{{ $containerInnerClasses ?? 'px-6' }}">
+                <div class="{{ $containerInnerClasses }}">
                     @yield('content')
                 </div>
             </main>
-
-            @includeIf('bonsai.sections.footer')
+            @include('bonsai.components.footer')
         </div>
 
         @php(do_action('get_footer'))
