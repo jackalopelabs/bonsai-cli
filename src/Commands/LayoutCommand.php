042fa1497dce50b345994656fc2a98d37738079f
Mason Lawlor <mason.lawlor@me.com>
2025-02-26 15:01:55 -0700

    Removed unused layouts (hardcoded), and analytics from LayoutCommand

--- src/Commands/LayoutCommand.php
+++ src/Commands/LayoutCommand.php
@@ -163,7 +163,6 @@ BLADE;
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @php(do_action('get_header'))
         @php(wp_head())
-        @include('bonsai.components.analytics')
         @include('utils.styles')
     </head>
 
