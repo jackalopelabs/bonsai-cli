84a872ed50c810506fd9eb8789d5bb5ac6010fea
Mason Lawlor <mason.lawlor@me.com>
2025-02-26 14:57:24 -0700

    Cypress layout: Removed analytics component

--- templates/layouts/template-cypress.blade.php
+++ templates/layouts/template-cypress.blade.php
@@ -17,7 +17,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @php(do_action('get_header'))
         @php(wp_head())
-        {{-- @include('bonsai.components.analytics') --}}
         @include('utils.styles')
     </head>
 
